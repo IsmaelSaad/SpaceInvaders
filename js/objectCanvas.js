586257d00df0ece56d9a779ea089ccf58139afa7
ISMAPC\isma treball <a24ismsaajdi@inspedralbes.cat>
2025-04-03 19:27:28 +0200

    Canvios deprecando el sistema de tamaño dinamico del canvas.

--- js/objectCanvas.js
+++ js/objectCanvas.js
@@ -4,8 +4,8 @@ function object(x, y, src) {
     this.img = new Image();
     this.img.src = src;
 
-    this.drawObject = function(context, ratio) {
-        context.drawImage(this.img, this.x * ratio[0], this.y * ratio[1] , this.img.naturalWidth * ratio[0], this.img.naturalHeight * ratio[1]);
+    this.drawObject = function(context) {
+        context.drawImage(this.img, this.x, this.y, this.img.naturalWidth, this.img.naturalHeight);
     }
 
     this.detectCollision = function(fobj) {
